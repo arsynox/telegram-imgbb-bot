8e6d704a17aa91437a240371faec7ae9892f4e6c
Arsynox <killerboyroy2008@gmail.com>
2025-06-07 17:52:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,15 +7,15 @@ SUBSCRIBE_CHANNEL = os.getenv("SUBSCRIBE_CHANNEL", "codexfusion")
 # Multiple imgbb API keys with server config and deletion periods
 SERVERS = {
     "Server1": {
-        "api_key": os.getenv("IMGBB_API_KEY_1", "your_imgbb_api_key_1"),
+        "api_key": os.getenv("IMGBB_API_KEY_1", "9519474fc492698dbb639aec847a1919"),
         "delete_after_days": 180  # 6 months
     },
     "Server2": {
-        "api_key": os.getenv("IMGBB_API_KEY_2", "your_imgbb_api_key_2"),
+        "api_key": os.getenv("IMGBB_API_KEY_2", "2d7842bb09c5229c7dcc4acdeb7bc401"),
         "delete_after_days": 30   # 1 month
     },
     "Server3": {
-        "api_key": os.getenv("IMGBB_API_KEY_3", "your_imgbb_api_key_3"),
+        "api_key": os.getenv("IMGBB_API_KEY_3", "5c3d4b0dd67e705eec4497a46d2ae421"),
         "delete_after_days": None # No auto delete
     },
 }
